e84927f7570a2e94a92fddc0bd67ea895583c5ba
ssk090 <shivanandasai.38@gmail.com>
2025-04-09 23:31:21 +0530

    feat: implement user authentication and feedback generation logic in Agent component

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,10 +1,12 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Interview = () => {
+const Interview = async () => {
+    const user = await getCurrentUser();
     return (
         <>
             <h3>Interview Generation</h3>
-            <Agent userName="you" userId="user1" type="generate" />
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
         </>
     )
 }
