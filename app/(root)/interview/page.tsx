679c21389800a7b7873c842c413dfd6f58dbe068
ssk090 <shivanandasai.38@gmail.com>
2025-04-09 18:03:09 +0530

    feat: add Interview and Agent components with UI and interaction logic

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/Agent";
+
+const Interview = () => {
+    return (
+        <>
+            <h3>Interview Generation</h3>
+            <Agent userName="you" userId="user1" type="generate" />
+        </>
+    )
+}
+export default Interview;
