165ff34ab9a6f090f675b8c0e5b91606f94cf08a
ssk090 <shivanandasai.38@gmail.com>
2025-04-09 15:38:49 +0530

    feat: add Firebase integration and tech stack display components
    - Added Firebase Admin SDK and client initialization files for backend and frontend.
    - Introduced DisplayTechIcons component to showcase technology stack icons.
    - Implemented InterviewCard component to display interview details, including tech stack and feedback.
    - Updated package.json to include new dependencies for Firebase and dayjs.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
-import {ReactNode} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode } from 'react'
 
-const RootLayout = ({children}: { children: ReactNode }) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className='flex items-center gap-2'>
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className='text-primary-100'>Prep AI</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
     )
 }
 export default RootLayout
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,47 @@
+import InterviewCard from '@/components/InterviewCard'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
     return (
-        <div>Home Page</div>
+        <>
+            <section className='card-cta'>
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get interview ready with AI-Powered Practice and Feedback</h2>
+                    <p>Practice on real interview questions and get instant feedback</p>
+
+                    <Button asChild className='btn-primary max-sm:w-full'>
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image src="/robot.png" alt="robot" height={400} width={400} className='max-sm:hidden' />
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    {/* <p>You haven&apos;t taken any interviews yet</p> */}
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+                </div>
+            </section>
+
+            <section className='flex flex-col gap-6 mt-8'>
+                <h2>Take an Interview</h2>
+                <div className="interviews-section">
+                    {/* <p>There are no interviews available</p> */}
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+                </div>
+
+            </section>
+        </>
     )
 }
 export default Page
