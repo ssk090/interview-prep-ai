5adf8d5948bde760d71b0e12c403463fac7ea091
ssk090 <shivanandasai.38@gmail.com>
2025-04-09 13:21:36 +0530

    feat: implement form handling with validation and UI components for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type {Metadata} from "next";
-import {Mona_Sans} from "next/font/google";
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
     variable: "--font-mona-sans",
@@ -13,17 +14,18 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en" className="dark">
-        <body
-            className={`${monaSans.className} antialiased pattern`}
-        >
-        {children}
-        </body>
+            <body
+                className={`${monaSans.className} antialiased pattern`}
+            >
+                {children}
+                <Toaster />
+            </body>
         </html>
     );
 }
