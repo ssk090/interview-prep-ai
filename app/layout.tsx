96299a086c3f0e0877882f2a16287e789774d4fb
ssk090 <shivanandasai.38@gmail.com>
2025-05-01 21:51:41 +0530

    feat: add Vercel Analytics for improved tracking and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const monaSans = Mona_Sans({
     variable: "--font-mona-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({
             >
                 {children}
                 <Toaster />
+                <Analytics />
             </body>
         </html>
     );
